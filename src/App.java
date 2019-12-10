97e34e5891080f8e71375654dc59b2b8e29cb242
Antony Correia <antony.correia@grtgaz.com>
2019-12-10 11:44:22 +0100

    genericite done

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
 
   public static void main(String[] args) {
-
+    Solo<Integer> val = new Solo<Integer>(12);
+    int nombre =  val.getValeur();
   }
 }
--- src/Solo.java
+++ src/Solo.java
@@ -1,3 +1,19 @@
-public class Solo {
+public class Solo<T> {
+private T valeur;
 
+  public Solo() {
+    this.valeur = null;
+  }
+
+  public Solo(T valeur) {
+    this.valeur = valeur;
+  }
+
+  public T getValeur() {
+    return valeur;
+  }
+
+  public void setValeur(T valeur) {
+    this.valeur = valeur;
+  }
 }
